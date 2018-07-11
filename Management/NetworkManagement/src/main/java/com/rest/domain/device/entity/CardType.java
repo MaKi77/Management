184ae37f963891b32d14dd26d6ab835926cf5667
MaKi77 <Comarch@10.132.226.165>
2018-07-11 18:25:55 +0200

    Auto-generated data such as id or identifier is now ignored during POST

--- Management/NetworkManagement/src/main/java/com/rest/domain/device/entity/CardType.java
+++ Management/NetworkManagement/src/main/java/com/rest/domain/device/entity/CardType.java
@@ -3,17 +3,25 @@ package com.rest.domain.device.entity;
 import java.util.Arrays;
 
 public enum CardType {
-	TYPE_A,
-	TYPE_B,
-	TYPE_C;
-	
+	TYPE_A, TYPE_B, TYPE_C;
+
 	public static CardType fromString(String type) {
-		for(CardType enumValue : CardType.values()) {
-			if(enumValue.toString().equalsIgnoreCase(type)) {
+
+		for (CardType enumValue : CardType.values()) {
+			if (enumValue.toString().equalsIgnoreCase(type)) {
 				return enumValue;
 			}
 		}
-		throw new IllegalArgumentException(
-				type + " is not a valid card type. Supported values are: " + Arrays.asList(CardType.values()));
+
+		if (isTypeNotSupplied(type)) {
+			return null;
+		} else {
+			throw new IllegalArgumentException(
+					type + " is not a valid card type. Supported values are: " + Arrays.asList(CardType.values()));
+		}
+	}
+
+	static private boolean isTypeNotSupplied(String type) {
+		return type == null;
 	}
 }
--- Management/NetworkManagement/src/main/java/com/rest/domain/device/entity/DeviceType.java
+++ Management/NetworkManagement/src/main/java/com/rest/domain/device/entity/DeviceType.java
@@ -3,19 +3,24 @@ package com.rest.domain.device.entity;
 import java.util.Arrays;
 
 public enum DeviceType {
-	ROUTER, 
-	MODEM, 
-	SWITCHER, 
-	REPEATER, 
-	BRIDGE;
+	ROUTER, MODEM, SWITCHER, REPEATER, BRIDGE;
 
 	public static DeviceType fromString(String type) {
-		for(DeviceType enumValue : DeviceType.values()) {
-			if(enumValue.toString().equalsIgnoreCase(type)) {
+		for (DeviceType enumValue : DeviceType.values()) {
+			if (enumValue.toString().equalsIgnoreCase(type)) {
 				return enumValue;
 			}
 		}
-		throw new IllegalArgumentException(
-				type + " is not valid device type. Supported values are: " + Arrays.asList(DeviceType.values()));
+
+		if (isTypeNotSupplied(type)) {
+			return null;
+		} else {
+			throw new IllegalArgumentException(
+					type + " is not valid device type. Supported values are: " + Arrays.asList(DeviceType.values()));
+		}
+	}
+
+	static private boolean isTypeNotSupplied(String type) {
+		return type == null;
 	}
 };
